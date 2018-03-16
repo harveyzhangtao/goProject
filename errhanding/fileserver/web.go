c61db0dd10b5c7c3369308e8d3421baf3b855fb1
张涛 <zhangtao@fruitday.com>
2018-03-16 18:05:48 +0800

    panic and recover

--- errhanding/fileserver/web.go
+++ errhanding/fileserver/web.go
@@ -2,19 +2,41 @@ package main
 
 import (
 	"fmt"
-	"goProject/errhanding/fileserver/filelisting"
 	"net/http"
 	"os"
 
-	"github.com/gpmgo/gopm/modules/log"
+	"goProject/errhanding/fileserver/filelisting"
+	"log"
 )
 
+func tryRevover() {
+	defer func() {
+		r := recover()
+		if err, ok := r.(error); ok {
+			fmt.Println("error code:", err)
+		} else {
+			panic(r)
+		}
+	}()
+	//panic(errors.New("this is en error"))
+	//b := 0
+	//a := 5 / b
+	//fmt.Println(a)
+	panic(112)
+}
+
 func main() {
-	http.HandleFunc("/list/", errWrapper(filelisting.Handlist))
+	http.HandleFunc("/", errWrapper(filelisting.Handlist))
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		panic(err)
 	}
+	//tryRevover()
+}
+
+type userError interface {
+	error
+	Message() string
 }
 
 type appHandler func(writer http.ResponseWriter, request *http.Request) error
@@ -22,16 +44,29 @@ type appHandler func(writer http.ResponseWriter, request *http.Request) error
 func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 	fmt.Println(handler)
 	return func(writer http.ResponseWriter, request *http.Request) {
+
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("Panic : %v", r)
+				http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+
+		}()
+
 		err := handler(writer, request)
 		if err != nil {
-			log.Warn("Error handing request:%s", err)
+			log.Printf("Error handing request:%s", err)
+
+			if userErr, ok := err.(userError); ok {
+				http.Error(writer, userErr.Message(), http.StatusBadRequest)
+				return
+			}
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
 				code = http.StatusNotFound
 			case os.IsPermission(err):
 				code = http.StatusForbidden
-
 			default:
 				code = http.StatusInternalServerError
 			}
